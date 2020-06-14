6efcdd59093bc93dd639ee50e23e37d7bebe6018
tiff0000 <tiffany.yang@mail.utoronto.ca>
2020-06-14 17:35:45 -0400

    added build folder

---
+++ build/precache-manifest.d2c2a3c6538f53ddd3160a0342b0e258.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee5a6ae8cfe664f8ee4b2422a97ccc18",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bfbeb9c1c7ef2496967d",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "aa7071c23e226124416d",
+    "url": "/static/js/2.62509fce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.62509fce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfbeb9c1c7ef2496967d",
+    "url": "/static/js/main.7323a578.chunk.js"
+  },
+  {
+    "revision": "e1746ec167b7f3f71a57",
+    "url": "/static/js/runtime-main.6c806260.js"
+  }
+]);
